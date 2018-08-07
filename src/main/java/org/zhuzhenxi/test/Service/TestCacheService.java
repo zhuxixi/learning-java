41e619481cb28a52e9970a9aacf90064e9d87b8b
zhuzhenxi <zzx2017@e-chinalife.com>
2018-08-07 16:37:49 +0800

    增加了cacheRefresh

--- src/main/java/org/zhuzhenxi/test/Service/TestCacheService.java
+++ src/main/java/org/zhuzhenxi/test/Service/TestCacheService.java
@@ -1,18 +1,29 @@
 package org.zhuzhenxi.test.Service;
 
+import com.alicp.jetcache.anno.CacheRefresh;
 import com.alicp.jetcache.anno.CacheType;
 import com.alicp.jetcache.anno.CacheUpdate;
 import com.alicp.jetcache.anno.Cached;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+/**
+ * JetCache学习笔记
+ * @author zhuzhenxi
+ * @date 2018.06.07
+ */
 public interface TestCacheService {
+
 
     String queryValue(String param);
 
+
     String putValue(String param);
 
+
     List<String> queryMultipleValue(String param1, String param2);
+
 
     List<String> putMultipleValue(String param1,String param2);
 }
--- src/main/java/org/zhuzhenxi/test/Service/impl/TestCacheServiceImpl.java
+++ src/main/java/org/zhuzhenxi/test/Service/impl/TestCacheServiceImpl.java
@@ -10,41 +10,65 @@ import org.zhuzhenxi.test.Service.TestCacheService;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Timer;
 import java.util.concurrent.TimeUnit;
 
 @Service
-@CacheConfig
 public class TestCacheServiceImpl implements TestCacheService {
 
     private static String value = "";
 
     private static List<String> valueList = new ArrayList<>();
 
-    @Cached(name="testCache-", key="#param",expire = 1,timeUnit = TimeUnit.MINUTES,cacheType = CacheType.LOCAL,area = "default")
-    @CacheRefresh(refresh = 4,timeUnit = TimeUnit.SECONDS)
+    /**
+     * 单参数
+     * 缓存过期时间 3min
+     * 自动刷新时间间隔 2min
+     * Key最后一次读取后2min停止刷新
+     * 个人认为：缓存刷新时间间隔=停止刷新时间<缓存过期时间
+     * @param param
+     * @return
+     */
+    @Cached(name="testCache", key="'queryValue' + ':' + #param",expire = 3,timeUnit = TimeUnit.MINUTES,cacheType = CacheType.LOCAL,area = "default")
+    @CacheRefresh(refresh = 2,stopRefreshAfterLastAccess=2,timeUnit = TimeUnit.MINUTES)
     @Override
     public String queryValue(String param) {
         System.out.println("进入了queryValue方法体");
         System.out.println("当前数据是"+value);
         return value;
     }
-    @CacheUpdate(name="testCache-", key="#param",value = "#result",area = "default")
+    /**
+     * 单参数
+     * 过期时间
+     * @param param
+     * @return
+     */
+    @CacheUpdate(name="testCache", key="#root.targetClass.simpleName + ':' + #param",value = "#result",area = "default")
     @Override
     public String putValue(String param) {
         System.out.println("进入了putValue方法体");
         value = param+"321321321";
         return value;
     }
-
-    @Cached(name="testCache-", key="#param1+#param2",expire = 1,timeUnit = TimeUnit.MINUTES,cacheType = CacheType.LOCAL,area = "default")
+    /**
+     * 双参数组合Key
+     * @param param1
+     * @param param2
+     * @return
+     */
+    @Cached(name="testCache", key="#param1+#param2",expire = 1,timeUnit = TimeUnit.MINUTES,cacheType = CacheType.LOCAL,area = "default")
     @Override
     public List<String> queryMultipleValue(String param1, String param2) {
         System.out.println("进入了queryMultipleValue方法体");
         return valueList;
     }
 
-    @CacheUpdate(name="testCache-", key="#param1+#param2",value = "#result",area = "default")
+    /**
+     * 双参数组合Key
+     * @param param1
+     * @param param2
+     * @return
+     */
+    @CacheUpdate(name="testCache", key="#param1+#param2",value = "#result",area = "default")
     @Override
     public List<String> putMultipleValue(String param1, String param2) {
         System.out.println("进入了putMultipleValue方法体");
