f74a6d1b6806a64993119b2d50ffa87443bc8458
zhuzhenxi <zzx2017@e-chinalife.com>
2018-11-21 10:42:49 +0800

    报文生成器

--- src/main/java/org/zhuzhenxi/test/file/FileUtil.java
+++ src/main/java/org/zhuzhenxi/test/file/FileUtil.java
@@ -1,5 +1,7 @@
 package org.zhuzhenxi.test.file;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import org.zhuzhenxi.test.random.RandomUtil;
 
 import java.io.*;
@@ -33,6 +35,31 @@ public class FileUtil {
         return stringList;
 
     }
+
+    public static List<String> readFileByPath(String filePath){
+        File file = new File(filePath);
+        Long fileLength = file.length(); // 获取文件长度
+        byte[] filecontent = new byte[fileLength.intValue()];
+        try
+        {
+            FileInputStream in = new FileInputStream(file);
+            in.read(filecontent);
+            in.close();
+        } catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        String[] fileContentArr = new String(filecontent).split("\n");
+
+        List<String> stringList = Arrays.asList(fileContentArr);
+        return stringList;
+
+    }
+
     public static void writeFile(String fileName){
         File file2 = new File("/Users/zhuzhenxi/Downloads/testfile/"+fileName+".csv");
         Writer write = null;
@@ -41,6 +68,27 @@ public class FileUtil {
             for (int i = 0; i < 100000; i++) {
                 write.write(RandomUtil.generateRandomNumber() +"\n");
             }
+
+            write.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally{
+            if(write != null){
+                try {
+                    write.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    public static void writeFileByPath(String filePath, List<JSONObject> content){
+        File file2 = new File(filePath);
+        Writer write = null;
+        try {
+            write = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file2), "UTF-8"));
+            write.write(JSON.toJSONString(content) +"\n");
+
 
             write.flush();
         } catch (IOException e) {
--- src/main/java/org/zhuzhenxi/test/random/RandomUtil.java
+++ src/main/java/org/zhuzhenxi/test/random/RandomUtil.java
@@ -7,6 +7,20 @@ import java.util.Random;
  */
 public class RandomUtil {
 
+
+    /**
+     * 产生n位随机数
+     * @return
+     */
+    public static long generateRandomNumber(int n){
+        if(n<1){
+            throw new IllegalArgumentException("随机数位数必须大于0");
+        }
+        return (long)(Math.random()*9*Math.pow(10,n-1)) + (long)Math.pow(10,n-1);
+    }
+
+
+
     /**
      * 随机生成6位整数
      * @return
