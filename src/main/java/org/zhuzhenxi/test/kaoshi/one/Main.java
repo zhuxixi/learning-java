478de0ba44110510d76b4da45ec1c9df57e92ab3
18100534 <zzx2017@e-chinalife.com>
2019-04-02 15:35:31 +0800

    研发中心初级考试

--- src/main/java/org/zhuzhenxi/test/kaoshi/one/Main.java
+++ src/main/java/org/zhuzhenxi/test/kaoshi/one/Main.java
@@ -6,7 +6,10 @@ public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()){
-
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            System.out.println((a+b+c)*30);
         }
     }
 }
--- src/main/java/org/zhuzhenxi/test/kaoshi/three/Main.java
+++ src/main/java/org/zhuzhenxi/test/kaoshi/three/Main.java
@@ -6,7 +6,12 @@ public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()){
-
+            String line = sc.nextLine();
+            char[] chars = line.toCharArray();
+            for (int i = chars.length-1; i >=0 ; i--) {
+                System.out.print(chars[i]);
+            }
+            System.out.println();
         }
     }
 }
